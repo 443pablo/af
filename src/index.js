799ce40b37b09b69fd9ecf80d30ea206bcd8b194
443pablo <78528552+443pablo@users.noreply.github.com>
2025-08-07 23:00:31 -0700

    feat: rework bangs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import { instructions } from "./instructions.js";
 import { AI } from "./lib.js";
 import open from "open";
+import { bangs } from "./bangs.js";
+import { homedir } from "os";
 
 (async () => {
+  const configPath = `${homedir()}/.af_config.json`
   const prompt = process.argv[2] ?? process.argv[1];
-  const j = await AI.getWebsite(prompt);
+  const matchedBang = bangs.find(bang => bang.t === prompt);
+  const isBang = !!matchedBang;
+
+  let url;
+  
+
+  if (!isBang) {const j = await AI.getWebsite(prompt); url = j.content;} else {url = matchedBang.u;}
 
   //console.log("Prompt: " + prompt);
   //console.log("Response: " + j.content);
-  await open(j.content)
+  await open(url);
 })();
